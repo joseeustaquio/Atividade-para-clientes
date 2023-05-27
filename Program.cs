b6a3f66f761aed6c0490894fbe7c05cbcb30b9b1
Jose Eustaquio <taquinhomz@gmail.com>
2023-05-27 11:41:20 -0300

    Criado o objeto Pessoa Física

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace ClientLab
     {
         static void Main(string[] args)
         {
+            float valorPago;
+            Console.WriteLine("Informe o seu nome.");
+            string varNome = Console.ReadLine();
             
+             Console.WriteLine("Informe o seu endereço.");
+             string varEndereco = Console.ReadLine();
+            
+            Console.WriteLine("Pessoa física(f) ou PessoaJurídica(j)");
+            string varTipo = Console.ReadLine();
+
+            if(varTipo == "f")
+            {
+                PessoaFisica pFisica = new PessoaFisica();
+                pFisica.nome = varNome;
+                pFisica.endereco = varEndereco;
+
+               
+               Console.WriteLine("Informe o seu Cpf:");
+                pFisica.cpf = Console.ReadLine();
+
+                Console.WriteLine("Informe o seu Rg:");
+                pFisica.rg = Console.ReadLine();
+
+                Console.WriteLine("Informe o valor da compra:");
+                valorPago = float.Parse(Console.ReadLine());
+               
+                pFisica.pagarImposto(valorPago);
+                Console.WriteLine("-----Pessoa Física-----");
+                Console.WriteLine("Nome....."+pFisica.nome);
+                 Console.WriteLine("Endereço....."+pFisica.endereco);
+                  Console.WriteLine("CPF....."+pFisica.cpf);
+                   Console.WriteLine("RG....."+pFisica.rg);
+                    Console.WriteLine("Valor da compra....."+pFisica.valor);
+                     Console.WriteLine("Valor do imposto....."+pFisica.valorImposto);
+                      Console.WriteLine("Total a pagar....."+pFisica.total);
+                       Console.WriteLine("....."+);
+                        Console.WriteLine("....."+);
+
+
+            }
         }
     }
 }
